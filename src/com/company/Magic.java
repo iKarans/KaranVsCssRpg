fea590e2e95e8fc427d007056a6469d4f426fcc9
iKarans <68602829+iKarans@users.noreply.github.com>
2021-11-15 19:23:11 +0000

    created magic constructor and attributes

---
+++ src/com/company/Magic.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Magic {
+    private String name;
+    private int cost;
+    private int dmg;
+    private String type;
+
+    public Magic(String name, int cost, int dmg, String type) {
+        this.name = name;
+        this.cost = cost;
+        this.dmg = dmg;
+        this.type = type;
+    }
+}
