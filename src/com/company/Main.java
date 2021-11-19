53b871826a7c5bd13f5020ef8ddfa341f9f48426
iKarans <68602829+iKarans@users.noreply.github.com>
2021-11-19 14:13:15 +0000

    created magic instances

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
+        Magic fire = new Magic("Fire", 25, 600, "black");
+        Magic thunder = new Magic("Thunder", 25, 600, "black");
+        Magic blizzard = new Magic("Blizzard", 25, 600, "black");
+        Magic meteor = new Magic("Meteor", 40, 1200, "black");
+        Magic quake = new Magic("Quake", 14, 140, "black");
+
+        Magic cure = new Magic("Cure", 25, 620, "white");
+        Magic cura = new Magic("Cura", 32, 1500, "white");
+        Magic curaga = new Magic("Curaga", 50, 6000, "white");
+
     }
 }
